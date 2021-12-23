28f3424e3a245efacb7f8e8248728f89627889d3
Karllic <64030092+Karllic@users.noreply.github.com>
2021-12-23 16:52:53 +0800

    Update wifi-changed.js

--- wifi-changed.js
+++ wifi-changed.js
@@ -3,13 +3,13 @@ const CURRENT_WIFI_SSID_KEY = 'current_wifi_ssid';
 
 if (wifiChanged()) {
   const mode = WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)
-    ? 'direct'
-    : 'rule';
+    ? '直接连接'
+    : '规则判定';
   $surge.setOutboundMode(mode);
   $notification.post(
-    'Surge',
-    `Wi-Fi changed to ${$network.wifi.ssid || 'cellular'}`,
-    `Use ${mode} mode`
+    '网络已改变',
+    `${$network.wifi.ssid || '蜂窝网络'}`,
+    `现使用 ${mode} 模式`
   );
 }
 
